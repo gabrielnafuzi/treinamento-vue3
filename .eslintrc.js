519f99c53ff7d53ab1b4149cff9c25c1ef78f2a1
Gabriel Moraes <gabriel.moraes@ceresinvestimentos.com>
2021-02-04 20:18:41 -0300

    Initial commit

---
+++ .eslintrc.js
@@ -0,0 +1,28 @@
+module.exports = {
+  root: true,
+  env: {
+    node: true
+  },
+  extends: ['plugin:vue/vue3-essential', '@vue/standard'],
+  parserOptions: {
+    parser: 'babel-eslint'
+  },
+  rules: {
+    semi: [2, 'never'],
+    'no-extra-boolean-cast': 0,
+    'no-console': 1,
+    'vue/max-attributes-per-line': 'off',
+    'vue/singleline-html-element-content-newline': 'off'
+  },
+  overrides: [
+    {
+      files: [
+        '**/__tests__/*.{j,t}s?(x)',
+        '**/tests/unit/**/*.spec.{j,t}s?(x)'
+      ],
+      env: {
+        jest: true
+      }
+    }
+  ]
+}
---
+++ src/views/Home/index.vue
@@ -0,0 +1,7 @@
+<template>
+  <h1 class="text-brand-main">Aqui é a home</h1>
+</template>
+
+<script>
+export default {}
+</script>
